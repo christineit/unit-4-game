1d820e242f5699afbd95f5cc3b0086fe7d97341e
christineit <christineitrinh@gmail.com>
2019-03-05 00:02:29 -0600

    added html, css, and js content

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// alert("Select a gem to start the game!");
+
+$("#gem-image-1").on("click", function() {
+  alert("you clicked gem 1!");
+});
+$("#gem-image-2").on("click", function() {
+  alert("you clicked gem 2!");
+});
+
+$("#gem-image-3").on("click", function() {
+  alert("you clicked gem 3!");
+});
+$("#gem-image-4").on("click", function() {
+  alert("you clicked gem 4!");
+});
